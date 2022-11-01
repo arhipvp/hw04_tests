a812e6515f82a91d05f1a922b182e40802756955
Архипов Владимир <arhipvp@gmail.com>
2022-11-01 20:05:34 +0300

    01.11.2022 - 3

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
@@ -13,6 +13,7 @@ class ViewTests(TestCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        TEST_POSTS_COUNT = 50
         User.objects.create_user(username="TestUser")
         Group.objects.create(
             title='Тестовый заголовок',
@@ -24,18 +25,21 @@ class ViewTests(TestCase):
             description='Тестовый текст 2',
             slug='test-slug-2',
         )
-        for post in range(50):
+        for post in range(TEST_POSTS_COUNT):
             try:
-                group_id = str(
-                ((Group.objects.get(pk=random(Group.objects.count()))))
+                Post.objects.create(
+                    text='testtext' + str(post),
+                    author=User.objects.get(username='TestUser'),
+                    group=Group.objects.get(
+                        pk=random.randint(0, Group.objects.count())
+                    ),
                 )
-            except:
-                group_id = ''
-            Post.objects.create(
-                text='testtext' + str(post),
-                author=User.objects.get(username='TestUser'),
-                group_id=group_id
-            )
+            except Group.DoesNotExist:
+                Post.objects.create(
+                    text='testtext' + str(post),
+                    author=User.objects.get(username='TestUser'),
+                )
+
         Post.objects.create(
             id=100,
             text='TEST TEXTTEST TEXTTEST TEXTTEST TEXT',
@@ -53,22 +57,22 @@ class ViewTests(TestCase):
     def test_templates_view_auth(self):
         """(авторизован)Во view-функциях используются правильные шаблоны."""
         Templates_Reverse = {
-            'posts/index.html': reverse('posts:index'),
-            'posts/group_list.html': reverse(
+            reverse('posts:index'): 'posts/index.html',
+            reverse(
                 'posts:group_list', kwargs={'slug': 'test-slug'}
-            ),
-            'posts/profile.html': reverse(
+            ): 'posts/group_list.html',
+            reverse(
                 'posts:profile', kwargs={'username': 'TestUser'}
-            ),
-            'posts/post_detail.html': reverse(
+            ): 'posts/profile.html',
+            reverse(
                 'posts:post_detail', kwargs={'post_id': '100'}
-            ),
-            'posts/create_post.html': reverse(
+            ): 'posts/post_detail.html',
+            reverse(
                 'posts:post_edit', kwargs={'post_id': '100'}
-            ),
-            'posts/create_post.html': reverse('posts:post_create'),
+            ): 'posts/create_post.html',
+            reverse('posts:post_create'): 'posts/create_post.html',
         }
-        for template, reverse_view in Templates_Reverse.items():
+        for reverse_view, template in Templates_Reverse.items():
             with self.subTest(address=reverse_view):
                 response = self.authorized_client.get(reverse_view)
                 self.assertTemplateUsed(response, template)
@@ -96,26 +100,26 @@ class ViewTests(TestCase):
         """Если при создании поста указать группу, то появляется на главной"""
         response = self.authorized_client.get(reverse('posts:index'))
         self.assertContains(response, Post.objects.get(id=100).text)
-    
+
     def test_post_create_group(self):
-        """Проверяем, что если при создании поста указать группу, то этот пост появляется на странице группы"""
-        response = self.authorized_client.get(reverse('posts:group_list', kwargs={'slug': 'test-slug'}))
+        """Если при создании поста указать группу,
+        то этот пост появляется на странице группы"""
+        response = self.authorized_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'test-slug'})
+        )
         self.assertContains(response, Post.objects.get(id=100).text)
-    
+
     def test_post_create_profile(self):
-        """Проверяем, что если при создании поста указать группу, то этот пост появляется в профайле"""
-        response = self.authorized_client.get(reverse('posts:profile', kwargs={'username': 'TestUser'}))
+        """Если при создании поста указать группу,
+        то этот пост появляется в профайле"""
+        response = self.authorized_client.get(
+            reverse('posts:profile', kwargs={'username': 'TestUser'})
+        )
         self.assertContains(response, Post.objects.get(id=100).text)
-    
+
     def test_post_create_no_other_group(self):
         """Пост не попал в группу, для которой не был предназначен."""
-        response = self.authorized_client.get(reverse('posts:group_list', kwargs={'slug': 'test-slug-2'}))
+        response = self.authorized_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'test-slug-2'})
+        )
         self.assertNotContains(response, Post.objects.get(id=100).text)
-    
-
-
-    def test_template_context(self):
-        """Провеяем, соответствует ли ожиданиям словарь context, передаваемый в шаблон при вызове."""
-        pass
-
- 
